e2ed7f143d34dc5bfd540bc557738e6e400a7093
redfly8 <redfly8@outlook.com>
2022-01-25 19:24:34 +0100

    mayor 1.1.0
    new: ban,. kick, mute, unmute, help, welcome msg and welcome role

--- main.js
+++ main.js
@@ -1,38 +1,69 @@
+const { channel } = require('diagnostics_channel');
 const Discord = require('discord.js');
-const client = new Discord.Client();
- 
-const prefix = '?';
- 
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+
+const prefix = '-';
+
 const fs = require('fs');
- 
+const { CLIENT_RENEG_WINDOW } = require('tls');
+
 client.commands = new Discord.Collection();
- 
+
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
+for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
- 
+
     client.commands.set(command.name, command);
 }
- 
- 
+
+
 client.once('ready', () => {
     console.log('bot_online');
 });
- 
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
- 
+
+client.on('guildMemberAdd', guildMember =>{
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'member');
+    guildMember.roles.add(welcomeRole)
+    //guildMember.guild.channels.cache.get('931649159021330502').send('channel_welcome_msg') 
+    guildMember.send('welcome on heehaws hillbillys. Make sure to read the rules and have fun =) ')
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
- 
-    if(command === 'ping'){
-        client.commands.get('ping').execute(message, args);
-    }   else if(command == 'rules') {
-        client.commands.get('rules').execute(message, args, Discord);
-    } else if(command == 'magic8'){
-        client.commands.get('magic8').execute(message, args);
-    }
 
-});
+    if (command === 'ping') {
+        client.commands.get('ping').execute(message, args);
+    //} else if (command == 'youtube') {
+        //client.commands.get('youtube').execute(message, args);
+    //} else if (command == 'pkick') {
+       // client.commands.get('pkick').execute(message, args);
+    //} else if (command == 'command') {
+        //client.commands.get('command').execute(message, args, Discord);
+    //} else if (command == 'dm') {
+        //client.commands.get('dm').execute(message, args, Discord);
+    } else if (command == 'clear') {
+        client.commands.get('clear').execute(message, args);
+    } else if (command == 'kick') {
+        client.commands.get('kick').execute(message, args, Discord);
+    } else if (command == 'ban') {
+        client.commands.get('ban').execute(message, args, Discord);
+    } else if (command == 'mute') {
+        client.commands.get('mute').execute(message, args, Discord);
+    } else if (command == 'unmute') {
+        client.commands.get('unmute').execute(message, args, Discord);
+    //} else if (command == 'reactionping' ){
+        //client.commands.get('reactionping').execute(message, args, Discord, client);
+    } else if(command == 'help') {
+        client.commands.get('help').execute(message, args, Discord);
+    } else if(command == 'magic8') {
+        client.commands.get('magic8').execute(message, args);
+    } 
+})
+
+
+
  
 client.login('OTMxNTk2OTUxMDY0MDM1MzM4.YeGvRQ.Zpq3wc_wa8ahX7YsQAwBorJR9Ic');
