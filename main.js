772e651370e4bcb99068994aaeef60430060ed6d
redfly8 <redfly8@outlook.com>
2022-02-26 21:33:33 +0100

    token shit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { channel } = require('diagnostics_channel');
 const Discord = require('discord.js');
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
-
+require('dotenv').config();
 const prefix = '?';
 
 const fs = require('fs');
@@ -91,5 +91,5 @@ client.on('message', message => {
 
 
 
- 
-client.login(process.env.token);
+console.log(process.env.DISCORD_TOKEN)
+client.login(process.env.DISCORD_TOKEN);
