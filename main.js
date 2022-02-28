0a6f3f56859e62329b098b16e534bb9970325102
redfly <97805073+redfly8@users.noreply.github.com>
2022-02-28 11:04:41 +0000

    working.env

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const { channel } = require('diagnostics_channel');
 const Discord = require('discord.js');
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
@@ -91,5 +92,6 @@ client.on('message', message => {
 
 
 
-console.log(process.env.BOT_TOKEN)
+console.log(process.env['BOT_TOKEN'])
+client.login(process.env['BOT_TOKEN'])
 //client.login(process.env.BOT_TOKEN);
